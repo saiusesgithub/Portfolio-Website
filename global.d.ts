3521680e28996c41bc2353b8f859af653c681417
saiusesgithub <saisrujanpunati@gmail.com>
2026-01-13 16:50:15 +0530

    feat: add type declarations for meshline integration with react-three-fiber

--- global.d.ts
+++ global.d.ts
@@ -9,17 +9,3 @@ declare module '*.png' {
   const content: any;
   export default content;
 }
-
-declare module 'meshline' {
-  export const MeshLineGeometry: any;
-  export const MeshLineMaterial: any;
-}
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      meshLineGeometry: any;
-      meshLineMaterial: any;
-    }
-  }
-}
